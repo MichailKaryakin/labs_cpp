bbaaa95b3a0867f4620cfe2c4e6d6e4009bc0bfd
Your Name <you@example.com>
2021-03-28 18:17:05 +0300

    refactor

--- lab_6/src/String.h
+++ lab_6/src/String.h
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <cctype>
+#include <cmath>
 
 class String {
 protected:
@@ -144,6 +145,36 @@ public:
 
 class StringBin : public String {
 private:
+    struct Array {
+        int size;
+        int cap;
+        char* data;
+    };
+
+    static Array NewArray(int cap) {
+        Array mas{};
+        mas.cap = cap;
+        mas.size = 0;
+        mas.data = (char*) malloc(mas.cap * sizeof(char));
+        return mas;
+    }
+
+    static void Add(Array* mas, char new_val) {
+        mas->size++;
+        if (mas->size < mas->cap) {
+            mas->data[mas->size] = new_val;
+        } else {
+            char* new_mas = (char*) malloc((mas->cap * 2) * sizeof(char));
+            for (int j = 0; j < mas->size - 1; ++j) {
+                new_mas[j] = mas->data[j];
+            }
+            free(mas->data);
+            mas->data = new_mas;
+            mas->cap = (int) (mas->cap * 2);
+            mas->data[mas->size - 1] = new_val;
+        }
+    }
+
     static bool isBin(char* str) {
         for (int i = 0; i < strlen(str); ++i) {
             if ((str[i] - 48) != 0 && (str[i] - 48) != 1) {
@@ -152,9 +183,81 @@ private:
         }
         return true;
     };
+
+    static char* Invert_Bin(char* str) {
+        for (int j = 1; j < strlen(str); ++j) {
+            if (str[j] == 48) {
+                str[j] = 49;
+            } else {
+                str[j] = 48;
+            }
+        }
+        return str - 1;
+    }
+
+    static char* toStraight(char* str) {
+        int i = strlen(str) - 1;
+        if (str[i] == 48) {
+            int k = i;
+            while (str[k] != 49 && k != 0) {
+                str[k] = 49;
+                --k;
+            }
+            str[k] = 48;
+        } else {
+            str[i] = 48;
+        }
+        return Invert_Bin(str + 1);
+    }
+
+    int toDecimal(char* str) {
+        int number = 0;
+        int order = 0;
+        if (str[0] == 1) {
+            str = toStraight(str);
+        }
+        for (int i = strlen(str) - 1; i > 0; --i) {
+            number += (str[i] - 48) * (int) pow(2, order);
+            ++order;
+        }
+        if (str[0] == 1) {
+            number = -number;
+        }
+        return number;
+    }
+
+    static void Invert_string(char* str) {
+        int current_symbol_index = 0;
+        while (current_symbol_index + 1 <= strlen(str) / 2) {
+            char temp = str[current_symbol_index];
+            str[current_symbol_index] = str[strlen(str) - current_symbol_index + 1];
+            str[strlen(str) - current_symbol_index + 1] = temp;
+            ++current_symbol_index;
+        }
+    }
+
+    char* toBin(int number) {
+        Array array = NewArray(5);
+        if (number < 0) {
+            Add(&array, 49);
+        } else {
+            Add(&array, 48);
+        }
+        while (number > 0) {
+            Add(&array, number % 2 + 48);
+            number /= 2;
+        }
+        Invert_string(array.data + 1);
+        if (array.data[0] == 1) {
+            //инвертировать, циклически прибавить единицу и возвратить
+        } else {
+            return array.data;
+        }
+    }
+
 public:
-    explicit StringBin(char* str) : String() {
-        if (isBin(this->data)) {
+    explicit StringBin(char* str) : String(str) {
+        if (!isBin(this->data)) {
             this->length = 0;
             this->data = nullptr;
         }
@@ -173,11 +276,15 @@ public:
             char temp = this->data[current_symbol_index];
             this->data[current_symbol_index] = this->data[this->length - current_symbol_index + 1];
             this->data[this->length - current_symbol_index + 1] = temp;
+            ++current_symbol_index;
         }
     }
 
     StringBin& operator-(char* str) {
-
+        int first_number = toDecimal(this->data);
+        int second_number = toDecimal(str);
+        this->data = toBin(first_number - second_number);
+        return *this;
     }
 
     StringBin& operator=(char* str) override {
