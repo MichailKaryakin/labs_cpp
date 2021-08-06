cdc07f7131c43981496e20ae88c321ec5afcd5ef
MichailKaryakin <71598648+MichailKaryakin@users.noreply.github.com>
2021-08-06 20:57:03 +0500

    window created

--- game/main.cpp
+++ game/main.cpp
@@ -1,25 +1,6 @@
-#include <SDL2/SDL.h>
-#include <iostream>
-#include <cstdio>
+#include "headers/graphics.h"
 
 int main(int argc, char* argv[]) {
-    SDL_Window* window;
-
-    SDL_Init(SDL_INIT_VIDEO);
-
-    window = SDL_CreateWindow(
-            "FUCK YEAH",
-            SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            640,
-            480,
-            SDL_WINDOW_OPENGL
-    );
-
-    SDL_Delay(3000);
-
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
+    Graphics window;
     return 0;
 }
