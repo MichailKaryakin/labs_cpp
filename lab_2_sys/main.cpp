47e480d1f66b2468937c27d2689955f79e854415
Michail <you@example.com>
2022-05-26 17:06:17 +0300

    refactor

--- lab_2_sys/main.cpp
+++ lab_2_sys/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <winsock2.h>
 
+#define offset 48
+
 using namespace std;
 
 int main() {
@@ -30,10 +32,21 @@ int main() {
         // массив пакета протокола
         char message[] = "d";
         unsigned int messageLength = strlen(message);
-        unsigned int array[5] = {0, 0, 'e', 0, 65};
+        unsigned int array[5] = {0, 0, messageLength, 0, 65};
+        string container;
+        container += to_string(array[0]);
+        container += to_string(array[1]);
+        container += array[2] + offset;
+        container += to_string(array[3]);
+        container += array[4];
+        container += message;
 
         // отправка на сервер и получение ответа
-        send(sock, (char*) array, 3, 0);
+        send(sock, container.c_str(), container.length(), 0);
+        recv(sock, szBuffer, 30, 0);
+
+        // вывод сообщения с сервера
+        printf("Message from server: %s\n", szBuffer + 5);
 
         // закрытие сокета
         closesocket(sock);
@@ -45,10 +58,100 @@ int main() {
     }
 
     if (user_choice == 2) {
+        // начало работы
+        WSADATA wsaData;
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+        // настройка сокета
+        SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        sockaddr_in sockAddr{};
+        memset(&sockAddr, 0, sizeof(sockAddr));
+        sockAddr.sin_family = PF_INET;
+        sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        sockAddr.sin_port = htons(502);
+        connect(sock, (SOCKADDR*) &sockAddr, sizeof(SOCKADDR));
+
+        // буфер приёма
+        char szBuffer[30] = {0};
+
+        // массив пакета протокола
+        char message[] = "Michail";
+        unsigned int messageLength = strlen(message);
+        unsigned int array[5] = {0, 0, messageLength, 0, 67};
+        string container;
+        container += to_string(array[0]);
+        container += to_string(array[1]);
+        container += array[2] + offset;
+        container += to_string(array[3]);
+        container += array[4];
+        container += message;
+
+        // отправка на сервер и получение ответа
+        send(sock, container.c_str(), container.length(), 0);
+        recv(sock, szBuffer, 30, 0);
+
+        // вывод сообщения с сервера
+        printf("Message from server: %s\n", szBuffer + 5);
+
+        // закрытие сокета
+        closesocket(sock);
+
+        // завершение работы
+        WSACleanup();
+        system("pause");
+        return 0;
     }
 
     if (user_choice == 3) {
+        // получение чисел от пользователя
+        int firstNumber;
+        int secondNumber;
+        cout << "enter two numbers: " << endl;
+        cin >> firstNumber >> secondNumber;
 
+        // начало работы
+        WSADATA wsaData;
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+        // настройка сокета
+        SOCKET sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+        sockaddr_in sockAddr{};
+        memset(&sockAddr, 0, sizeof(sockAddr));
+        sockAddr.sin_family = PF_INET;
+        sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        sockAddr.sin_port = htons(502);
+        connect(sock, (SOCKADDR*) &sockAddr, sizeof(SOCKADDR));
+
+        // буфер приёма
+        char szBuffer[30] = {0};
+
+        // массив пакета протокола
+        string message;
+        message += to_string(firstNumber);
+        message += ' ';
+        message += to_string(secondNumber);
+        unsigned int array[5] = {0, 0, message.length(), 0, 69};
+        string container;
+        container += to_string(array[0]);
+        container += to_string(array[1]);
+        container += array[2] + offset;
+        container += to_string(array[3]);
+        container += array[4];
+        container += message;
+
+        // отправка на сервер и получение ответа
+        send(sock, container.c_str(), container.length(), 0);
+        recv(sock, szBuffer, 30, 0);
+
+        // вывод сообщения с сервера
+        printf("Message from server: %s\n", szBuffer + 5);
+
+        // закрытие сокета
+        closesocket(sock);
+
+        // завершение работы
+        WSACleanup();
+        system("pause");
+        return 0;
     }
 }
--- lab_2_sys_serv/main.cpp
+++ lab_2_sys_serv/main.cpp
@@ -1,6 +1,8 @@
 #include <winsock2.h>
 #include <iostream>
 
+#define offset 48
+
 using namespace std;
 
 int main() {
@@ -30,16 +32,32 @@ int main() {
         SOCKADDR clientAddr;
         int nSize;
         string response = "Invalid Function Code!";
-        char szBuffer[30];
+        char szBuffer[30] = {0};
         SOCKET clientSock;
 
         // Отправляем данные клиенту
         while (true) {
             nSize = sizeof(SOCKADDR);
             clientSock = accept(servSock, (SOCKADDR*) &clientAddr, &nSize);
+
             printf("work\n");
             recv(clientSock, szBuffer, 30, 0);
             printf("Message:%s\n", szBuffer);
+
+            if (szBuffer[4] == 'A') {
+                response = "connection completed";
+            }
+
+            unsigned int array[5] = {0, 0, response.length(), 0, 65};
+            string container;
+            container += to_string(array[0]);
+            container += to_string(array[1]);
+            container += array[2] + offset;
+            container += to_string(array[3]);
+            container += array[4];
+            container += response;
+
+            send(clientSock, container.c_str(), container.length(), 0);
         }
 
         // Закрываем сокет
@@ -53,10 +71,140 @@ int main() {
     }
 
     if (user_choice == 2) {
+        // Инициализируем DLL
+        WSADATA wsaData;
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+        // Создаем сокет
+        SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        // Привязываем сокет
+        sockaddr_in sockAddr;
+        memset(&sockAddr, 0, sizeof(sockAddr)); // Каждый байт заполняется 0
+        sockAddr.sin_family = PF_INET; // Использовать IPv4-адрес
+        sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Определенный IP-адрес
+        sockAddr.sin_port = htons(502); // Порт
+        bind(servSock, (SOCKADDR*) &sockAddr, sizeof(SOCKADDR));
+
+        // Входим в состояние мониторинга
+        listen(servSock, 20);
+
+        // Подготовка полей
+        SOCKADDR clientAddr;
+        int nSize;
+        string response = "Invalid Function Code!";
+        char szBuffer[30] = {0};
+        SOCKET clientSock;
+
+        // Отправляем данные клиенту
+        while (true) {
+            nSize = sizeof(SOCKADDR);
+            clientSock = accept(servSock, (SOCKADDR*) &clientAddr, &nSize);
+
+            printf("work\n");
+            recv(clientSock, szBuffer, 30, 0);
+            printf("Message:%s\n", szBuffer);
+
+            if (szBuffer[4] == 'C') {
+                response = "Hello, ";
+                response += (szBuffer + 5);
+            }
+
+            unsigned int array[5] = {0, 0, response.length(), 0, 65};
+            string container;
+            container += to_string(array[0]);
+            container += to_string(array[1]);
+            container += array[2] + offset;
+            container += to_string(array[3]);
+            container += array[4];
+            container += response;
+
+            send(clientSock, container.c_str(), container.length(), 0);
+        }
+
+        // Закрываем сокет
+        closesocket(clientSock);
+        closesocket(servSock);
+
+        // Прекращаем использование DLL
+        WSACleanup();
+
+        return 0;
     }
 
     if (user_choice == 3) {
+        // Инициализируем DLL
+        WSADATA wsaData;
+        WSAStartup(MAKEWORD(2, 2), &wsaData);
 
+        // Создаем сокет
+        SOCKET servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+        // Привязываем сокет
+        sockaddr_in sockAddr;
+        memset(&sockAddr, 0, sizeof(sockAddr)); // Каждый байт заполняется 0
+        sockAddr.sin_family = PF_INET; // Использовать IPv4-адрес
+        sockAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Определенный IP-адрес
+        sockAddr.sin_port = htons(502); // Порт
+        bind(servSock, (SOCKADDR*) &sockAddr, sizeof(SOCKADDR));
+
+        // Входим в состояние мониторинга
+        listen(servSock, 20);
+
+        // Подготовка полей
+        SOCKADDR clientAddr;
+        int nSize;
+        string response = "Invalid Function Code!";
+        char szBuffer[30] = {0};
+        SOCKET clientSock;
+
+        // Отправляем данные клиенту
+        while (true) {
+            nSize = sizeof(SOCKADDR);
+            clientSock = accept(servSock, (SOCKADDR*) &clientAddr, &nSize);
+
+            printf("work\n");
+            recv(clientSock, szBuffer, 30, 0);
+            printf("Message:%s\n", szBuffer);
+
+            if (szBuffer[4] == 'E') {
+                int firstNumber;
+                int secondNumber;
+
+                string numbers;
+                numbers += (szBuffer + 5);
+                string delimiter = " ";
+                size_t pos = 0;
+                string token;
+                while ((pos = numbers.find(delimiter)) != string::npos) {
+                    token = numbers.substr(0, pos);
+                    firstNumber = stoi(token);
+                    numbers.erase(0, pos + delimiter.length());
+                }
+                secondNumber = stoi(numbers);
+
+                response = "the sum of numbers - " + to_string(firstNumber + secondNumber);
+            }
+
+            unsigned int array[5] = {0, 0, response.length(), 0, 65};
+            string container;
+            container += to_string(array[0]);
+            container += to_string(array[1]);
+            container += array[2] + offset;
+            container += to_string(array[3]);
+            container += array[4];
+            container += response;
+
+            send(clientSock, container.c_str(), container.length(), 0);
+        }
+
+        // Закрываем сокет
+        closesocket(clientSock);
+        closesocket(servSock);
+
+        // Прекращаем использование DLL
+        WSACleanup();
+
+        return 0;
     }
 }
